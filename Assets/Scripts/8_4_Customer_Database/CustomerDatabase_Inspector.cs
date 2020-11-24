c515556a80ced4ea5eada6a957e31a3ef122206e
jmacnut <jmacnut@gmail.com>
2020-11-23 23:07:44 -1000

    completes Classes

---
+++ Assets/Scripts/8_4_Customer_Database/CustomerDatabase_Inspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CustomerDatabase
+{
+    public class CustomerDatabase_Inspector : MonoBehaviour
+    {
+        [SerializeField] private Customer[] customerDatabase;
+
+        private void Start()
+        {
+            foreach (var customer in customerDatabase)
+            {
+                customer.PrintCustomer();
+            }
+        }
+
+    }
+}
